6dc4c1ca1104213bd49a2e32473a7d687e163e20
Megha Mallya <meghamallya1@gmail.com>
2019-03-29 06:57:16 -0500

    added react-router

--- frontend/src/screens/LoginPage.js
+++ frontend/src/screens/LoginPage.js
@@ -3,6 +3,11 @@ import Login from '../components/Login'
 import '../style/Login.css';
 
 class LoginPage extends Component {
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            children: nextProps.children
+        });
+    }
     render() {
         return (
             <div className="login-page">
