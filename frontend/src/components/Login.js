5cfd8cf89ea9b287109aae8611ba07644a04fb7a
Megha Mallya <meghamallya1@gmail.com>
2019-03-29 07:03:14 -0500

    added routing to login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import LoginInput from './LoginInput'
 import '../style/Login.css';
+import { Route } from 'react-router-dom'
 
 class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            url :"/nphome"
+        }
+    }
     render() {
         return (
+            <Route render={({ history}) => (
             <div className="login-comp">
                 <img alt="Save Me A Piece" src={require('../logo.png')}/>
                 <div className="login-title">WELCOME BACK!</div>
@@ -12,10 +20,12 @@ class Login extends Component {
                 <br/>
                 <LoginInput icon="user" name="EMAIL"/>
                 <LoginInput icon="password" name="PASSWORD" />
-                <button className="login-button">
+                <button className="login-button" onClick={() => history.push(this.state.url)} > 
                     <span className="button-login-name">LOGIN</span>
                 </button>
             </div>
+            )}
+            />
         );
     }
 }
