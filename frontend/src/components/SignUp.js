34aa5443cc8def7ce163fdd838ca32317434d222
Hana Rimawi <hrimaw2@illinois.edu>
2019-04-18 02:24:42 -0500

    conecting

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import SignUpButton from './SignUpButton'
 import '../style/SignUp.css';
+import { Route } from 'react-router-dom';
+
 
 class SignUp extends Component {
+
+    handleRestButton = history => {
+        let path = `/login`;
+        history.push(path);
+    };
+
+    handleSoupButton = history => {
+        let path = `/login`;
+        history.push(path);
+    };
+
     render() {
         return (
+            <Route render={({history}) => (
             <div className="signup-comp">
                 <img alt="Save Me A Piece" src={require('../logo.png')}/>
                 <div className="signup-title">WELCOME!  </div>
                 <div className="signup-title">SELECT YOUR BUSINESS</div>
                 <br/>
-                <SignUpButton icon="restaurant" color="#8CB369" name="RESTAURANT"/>
-                <SignUpButton icon="soup" color="#D36582" name="SOUP KITCHEN"/>
+                    <SignUpButton onClick={history.push('/login')} icon="restaurant" color="#8CB369" name="RESTAURANT"/>
+                    <SignUpButton onClick={history.push('/login')} icon="soup" color="#D36582" name="SOUP KITCHEN"/>
+                )}/>
             </div>
+            )}/>
         );
     }
 }
